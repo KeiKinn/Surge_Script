a5ab8d05079deec399ad84b421fc8ac69ee3d891
JingX <jx589g@gmail.com>
2020-11-04 12:08:13 +0800

    Update Caixin.js

--- Surge_Script/Caixin.js
+++ Surge_Script/Caixin.js
@@ -1,8 +1,6 @@
 const url = $request.url;
 let body = $response.body;
 let paywallFlag = 'isPay > 0';
-let cssBottomMask = 'cons bottom-layer';
 
 body = body.replace(paywallFlag, 'false');
-body = body.replace(cssBottomMask, 'cons bottom_layer');
 $done({body});
