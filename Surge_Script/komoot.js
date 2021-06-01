bcededf917ad98df7c76265b533301352e581e58
KeiKinn <jx589g@gmail.com>
2021-06-01 21:47:20 +0200

    add new komoot file

---
+++ Surge_Script/komoot.js
@@ -0,0 +1,10 @@
+let url = $request.url
+let body = $response.body
+
+if (url.includes('config') || url.includes('embedded=premium')) {
+  body = body.replace('premium":fals', 'premium":tru');
+} else if (url.includes('poinner')) {
+  body = body.replace('is_premium": fals', 'is_premium": tru');
+}
+
+$done({body})
