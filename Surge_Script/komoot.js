0fcf0879f00c93124d5cf79d0311108decd21b46
Xin Jing <jx589g@gmail.com>
2025-03-20 12:48:23 +0100

    Update komoot.js

--- Surge_Script/komoot.js
+++ Surge_Script/komoot.js
@@ -3,7 +3,7 @@ let body = $response.body
 
 if (url.includes('config') || url.includes('embedded=premium')) {
   body = body.replace('premium":fals', 'premium":tru');
-} else if (url.includes('pioneer')) {
+} else if (url.includes('pioneer') || url.includes('activities')) {
   body = body.replace('is_premium": fals', 'is_premium": tru');
 } else if (url.includes("v006")){
   body = body.replace("NEEDSPURCHASE", "")
